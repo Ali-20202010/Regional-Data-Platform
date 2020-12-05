8d9547e327454b75bb770bbd9387d75b18ec3fce
Ali-20202010 <73132830+Ali-20202010@users.noreply.github.com>
2020-12-05 04:34:45 +0300

    Update jsLayers.js

--- js/jsLayers.js
+++ js/jsLayers.js
@@ -1,29 +1,12 @@
-    require([
+     require([
         "esri/Map",
         "esri/views/MapView",
-        "esri/layers/FeatureLayer",
-        "esri/widgets/Legend"
-      ], function (Map, MapView, FeatureLayer, Legend) {
-        const defaultSym = {
-          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-          color: [0, 0, 0, 0],
-          outline: {
-            // autocasts as new SimpleLineSymbol()
-            color: "#71de6e",
-            width: 1
-          }
-        };
-
-        /*****************************************************************
-         * Set a size visual variable on the renderer. Size visual variables
-         * create continuous ramps that map low data values to small icons
-         * and high data values to large icons. Features
-         * with data values in between the min and max data values are assigned
-         * a size proportionally between the min and max sizes specified in
-         * `minSize` and `maxSize` or `stops`.
-         *****************************************************************/
-
-   var pocPopup= {
+         "esri/layers/FeatureLayer",
+        "esri/widgets/Legend",
+        "esri/layers/TileLayer"
+      ], function (Map, MapView,FeatureLayer,Legend, TileLayer) {
+       
+        var pocPopup= {
         "title": "{iso3}",
         
         "content": "<b>Country :</b> {iso3}<br><b>Type of Camp:</b> {loc_type} <br><b>Location:</b> {name}<br><b> Created on :</b> {createdate}<br><b> Updated on :</b> {updatedate}<br><b> Source :</b> {source}<br><b> Footnote :</b> {footnote}"
@@ -33,6 +16,8 @@
         const pocLayer = new FeatureLayer({
           url:
             "https://gis.unhcr.org/arcgis/rest/services/core/wrl_ppl_poc_p_unhcr/MapServer/0/",
+             id: "poc-UNHCR",
+          visible: false,
          outFields: ["iso3","pcode","iso3","name","loc_type","updatedate"],
            fieldInfos: [
             {
@@ -45,46 +30,140 @@
       
         popupTemplate: pocPopup
         });
+        var chgTempLayer = new TileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/S9LAFnsxHsSpUJFQ/arcgis/rest/services/Chg_in_temperature_Midcentury_2046_2065/MapServer",
+          // This property can be used to uniquely identify the layer
+          id: "chgTemp",
+          visible: false
+        });
+        var tempLayer = new TileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/S9LAFnsxHsSpUJFQ/arcgis/rest/services/Temperature_1986_2005/MapServer",
+        
+          id: "temps",
+          visible: false
+        });
+            var landLayer = new TileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/S9LAFnsxHsSpUJFQ/arcgis/rest/services/Land_Use_Land_Cover_Arab_Region/MapServer",
+      
+          id: "lands",
+          visible: false
+        });
+        var cropLandLayer = new TileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/S9LAFnsxHsSpUJFQ/arcgis/rest/services/Irrigated_Croplands_Arab_Region/MapServer",
+          id: "Ar-cropLand",
+         visible: false
+        });
 
-        const map = new Map({
+        /*****************************************************************
+         * Layers may be added to the map in the map's constructor
+         *****************************************************************/
+        var map = new Map({
           basemap: "dark-gray-vector",
-          layers: [pocLayer]
+          layers: [pocLayer, cropLandLayer, chgTempLayer, tempLayer,landLayer]
         });
 
-        const view = new MapView({
+       
+
+        var view = new MapView({
           container: "viewDiv",
-          map: map,
-          center: [33.2232, 43.6793],
-          zoom: 3
+          map: map
         });
-		
-		
-	/*****
-		for hiding & displaying first layer on the map **/
-	
-	var checkbox = document.getElementById("Checkbox1");
-checkbox.onclick = function () {
-  pocLayer.visible = this.checked;
-}
-	
-
-
-        /******************************************************************
-         *
-         * Add layers to layerInfos on the legend
-         *
-         ******************************************************************/
-
-        view.ui.add(
+		        view.ui.add(
           new Legend({
             view: view
           }),
-          "top-left"
+          "top-right"
         );
+        
+        
+
+        view.on("layerview-create", function (event) {
+            if (event.layer.id === "poc-UNHCR") {
+           
+            console.log(
+              "LayerView for POC created!", event.layerView);
+          }
+          if (event.layer.id === "Ar-cropLand") {
+           
+            console.log(
+              "LayerView for Arab Crop Lands created!", event.layerView);
+          }
+          if (event.layer.id === "chgTemp") {
+           
+            console.log("LayerView for Tempreature 2046-2065 created!", event.layerView);
+          }
+            if (event.layer.id === "lands") {
+            
+            console.log("LayerView for Land use created!", event.layerView);
+          }
+          if (event.layer.id === "temps") {
+         
+            console.log("LayerView for temperature  created!", event.layerView);
+          }
+        });
+
+        view.when(function () {
+          cropLandLayer.when(function () {
+            view.goTo(cropLandLayer.fullExtent).catch(function (error) {
+              if (error.name != "AbortError") {
+                console.error(error);
+              }
+            });
+          });
+        });
+ var pocLayerToggle = document.getElementById("pocsLayer");
+
+        pocLayerToggle.addEventListener("change", function () {
+         pocLayer.visible = pocLayerToggle.checked;
+        });
+        var chgTempLayerToggle = document.getElementById("chgTempsLayer");
+
+       
+        chgTempLayerToggle.addEventListener("change", function () {
+         chgTempLayer.visible = chgTempLayerToggle.checked;
+        });
+            var landLayerToggle = document.getElementById("landsLayer");
+
+        
+        landLayerToggle.addEventListener("change", function () {
+          landLayer.visible = landLayerToggle.checked;
+        });
+
+        var tempLayerToggle = document.getElementById("tempsLayer");
+
+        tempLayerToggle.addEventListener("change", function () {
+          tempLayer.visible = tempLayerToggle.checked;
+        });
+        var cropLandLayerToggle = document.getElementById("cropLandsLayer");
+
+        /*****************************************************************
+         * The visible property on the layer can be used to toggle the
+         * layer's visibility in the view. When the visibility is turned off
+         * the layer is still part of the map, which means you can access
+         * its properties and perform analysis even though it isn't visible.
+         *******************************************************************/
+        cropLandLayerToggle.addEventListener("change", function () {
+          cropLandLayer.visible = cropLandLayerToggle.checked;
+        });
+		var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    
+  });
+}
       });
 
+
+/********************************************************************************************************************************/
 // displaying json data in table
-	$(document).ready(function() {
+	 $(document).ready(function() {
 
     $('#landings_table tfoot th').each( function () { // Change table element ID here
       var title = $('#landings_table thead th').eq( $(this).index() ).text(); // Change table element ID here
@@ -93,8 +172,8 @@ checkbox.onclick = function () {
    
     var url ="https://gis.unhcr.org/arcgis/rest/services/core/wrl_prp_p_unhcr/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=0.0&units=esriSRUnit_Meter&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=&resultOffset=&resultRecordCount=1000&returnTrueCurves=false&sqlFormat=none&f=pjson";  
 		var table = $('#landings_table').DataTable({ // Change table element ID here
-        dom: 'Bfrtip', // Add this to enable export buttons
-        buttons: [ // Add this to choose which buttons to display
+        dom: 'Bfrtip', 
+        buttons: [ 
             'csv', 'excel', 'pdf'
         ],
         "autoWidth": true, 
@@ -136,5 +215,4 @@ checkbox.onclick = function () {
           .draw();
       });
     }); 
-	});	
-	
+	});	 
